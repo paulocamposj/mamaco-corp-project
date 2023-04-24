509e292164afb75334a3d5b86c839bafe2128520
Paulo Campos <paulo.devcampos@gmail.com>
2023-04-24 10:27:26 -0300

    Componente Accordion

---
+++ force-app/main/default/aura/ComponenteAccordion/ComponenteAccordionHelper.js
@@ -0,0 +1,16 @@
+({
+    getData : function(cmp) {
+        var action = cmp.get('c.getLeads');
+        
+        action.setCallback(this, $A.getCallback(function (response) {
+            var state = response.getState();
+            if (state === "SUCCESS") {
+                cmp.set('v.mydata', response.getReturnValue());
+            } else if (state === "ERROR") {
+                var errors = response.getError();
+                console.error(errors);
+            }
+        }));
+        $A.enqueueAction(action);
+    }
+})
